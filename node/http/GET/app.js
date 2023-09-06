db937dd89c5f9fb73a3d31a834a9b082fca69e46
黄东 <huangdong23@meicloud.comgit config --global user.name >
2023-09-06 19:28:27 +0800

    feat: http

--- node/http/GET/app.js
+++ node/http/GET/app.js
@@ -1,19 +1,21 @@
 // http 处理 GET 请求
 const http = require('http');
 const qs = require('querystring');
-const opn = require('opn');
+const url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  const params = qs.parse(req.url.split('?')[1]);
+  // 方式一：通过 url 模块获取，url.parse(req.url, true);
+  // const query = url.parse(req.url, true).query;
+  // 方式二：通过 querystring 模块 qs.parse(req.url.split('?')[1]))
+  const query = qs.parse(req.url.split('?')[1]);
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end(JSON.stringify(params));
+  res.end(JSON.stringify(query));
 });
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-  opn(`http://${hostname}:${port}/`);
 });
